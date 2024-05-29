003efd9129a4843e9f6ffe4e270310b06975bf6e
Aidan Pratt <78280532+Panlos@users.noreply.github.com>
2024-05-28 21:22:45 -0600

    Week 3 Assignment
    Created item.js, item-list.js, page.js, the folder week-3 itself
    
    I created a shopping list of each item and have it displayed

---
+++ app/week-3/item.js
@@ -0,0 +1,13 @@
+import ItemList from "./item-list";
+
+const Item = ({ name, quantity, category }) => {
+    return (
+      <li className="p-4 mb-2 border rounded-lg shadow-md bg-white">
+        <div className="font-bold text-xl">{name}</div>
+        <div className="text-gray-700">Quantity: {quantity}</div>
+        <div className="text-gray-500">Category: {category}</div>
+      </li>
+    );
+  };
+
+export default Item;
---
+++ app/week-3/page.js
@@ -0,0 +1,13 @@
+import ItemList from "./item-list";
+import React from "react";
+
+export default function Page() {
+    return (
+      <main className="min-h-screen bg-amber-700 w-full">
+        <div>
+        <h1 className="bg-blue-950 text-3xl text-white">Shopping List</h1>
+        <ItemList />
+        </div>
+      </main>
+    );
+  }
